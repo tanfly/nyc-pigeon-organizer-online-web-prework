9075fc96d1e6a4a3f1f6580e99697b2074d93175
Stella McEwen <mcewen.stella@gmail.com>
2019-04-20 22:53:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -28,4 +28,5 @@ def nyc_pigeon_organizer(data)
   end
   data.map do |hash, info|
     info.map do |category, array|
-      pigeon_list.each do |info_array|
+      pigeon_list.each do |info_array, pigeon_name|
+        if array
